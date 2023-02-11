d12182458d51ef5f558c5d4404eec4f54f0adc86
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2023-02-11 10:26:28 +0530

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,50 +1,43 @@
 import React from 'react';
-import "./ContactForm.css"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Button,Label,Input,Desc,Title,Container,FormContainer,Divider,Right,Center,Left,Wrapper} from './ContactStyles'
 
-function ContactForm() {
+
+
+export default function ContactForm() {
   return (
-    <div className='box'>
-     
-     <Container>
-      <Row>
-        <Col sm={5}>
-            <div className='left'>
-            <h2>GET IN TOUCH</h2>
-            <p>Please Complete the Form we will <br /> get back to you.</p>
-            </div>
-        </Col>
-        <Col sm={7}>
-        <Form>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Name*</Form.Label>
-        <Form.Control type="name" placeholder="Enter Name" />
-       
-      </Form.Group>
+    <>
+    <Container>
+        <Wrapper>
+            <Left>
+            <Title>GET IN TOUCH</Title>
+            <Desc>Please complete the form and we will get back to you.</Desc>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email*</Form.Label>
-        <Form.Control type="email" placeholder="email" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formMobileNumber">
-        <Form.Label>Mobile Number</Form.Label>
-        <Form.Control type="MobileNumber" placeholder="MobileNumber" />
-      </Form.Group>
-      
-      <Button variant="primary" type="submit">
-        Register Now
-      </Button>
-    </Form>
-        </Col>
-      </Row>
+            </Left>
+            <Center>
+                <Divider></Divider>
+            </Center>
+            <Right>
+                <form>
+                <FormContainer>
+                    <Label>Name*</Label>
+                        <Input type={'text'} placeholder="Enter Your Name"></Input>
+                        
+                        <Label>Email*</Label>
+                        <Input type={'email'}placeholder="Enter Your Email"></Input>
+                     
+                        <Label>Phone Number*</Label>
+                        <Input type={'number'}  placeholder="Enter Your Number"></Input>
+                       
+                        <Button type="submit">Register Now</Button>
+                    </FormContainer>
+                </form>
+            </Right>
+            
+      </Wrapper>
     </Container>
-    </div>
+    </>
   )
 }
 
-export default ContactForm
+
+
